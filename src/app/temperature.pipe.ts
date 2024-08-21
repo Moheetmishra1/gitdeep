192b581cba2330d244e9effda1ab305e8e04aa72
Mohit Mishra <mohit.m@idexcel.com>
2024-08-21 19:40:09 +0530

    second cimmot

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -3,6 +3,18 @@ import { Pipe } from "@angular/core";
 @Pipe({
     name:"temperaturePipe",
     standalone:  true,
-    
+
 })
-export class TemperaturePipe{}
+export class TemperaturePipe{
+    transform(value:string|number){
+        let val:number;
+
+        if(typeof value === "string"){
+            val=parseFloat(value)
+        }else{
+            val = value;
+        }
+        let outputTem = val*(9/5) +32;
+        return `${outputTem} ^F`
+    }
+}
